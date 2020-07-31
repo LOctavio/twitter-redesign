ad80d3b3d012ad61bcc489f6bdcaee92e981d144
Luis <luis_ram1997@hotmail.com>
2020-07-31 17:38:52 -0500

    Create user model and add user table with columns

---
+++ db/migrate/20200731221332_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :full_name
+      t.string :photo
+      t.string :cover_image
+      t.datetime :created_at
+    end
+  end
+end
