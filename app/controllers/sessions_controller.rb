344273cf28bd823278722a627c530daec2ca7d09
Luis <luis_ram1997@hotmail.com>
2020-08-03 10:52:30 -0500

    Generate sessions controller and add methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+    def new; end
+    
+    def create
+        if @user = User.find_by(username: params[:session][:username])
+            session[:current_user_id] = @user.id
+            redirect_to root_url
+        end
+    end
+end
